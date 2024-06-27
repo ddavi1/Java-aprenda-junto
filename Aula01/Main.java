f4c671f60c1dfa8e75f2595ed4442ba9851d5499
ddavi1 <davigabriel324@gmail.com>
2024-06-27 18:19:16 -0300

    commit

---
+++ Aula01/Main.java
@@ -0,0 +1,13 @@
+package Aula01;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String nome = "davi";
+
+        System.out.println(nome);
+
+        
+    }
+}
---
+++ aula02/Aula02.java
@@ -0,0 +1,42 @@
+package aula02;
+
+public class Aula02 {
+
+
+    public static void main(String[] args) {
+        
+        // byte = armazena de -128 até 127 (%d)
+        byte pequeno = -128;
+
+        // short = armazena entre -32768 até 32767 (%d)
+        short quasePequeno = 32767;
+
+        // int = armazena entre -2147483648 até 2147483647 (%d)
+        int quaseGrande = -2147483648;
+
+        // long (necessita do L no final) = armazena entre 922337203685477000 (tanto Positivo quanto Negativo) (%d)
+        long grandao = 922337203685477000L;
+
+        // float (necessita do f no final) (%f)
+        float numeroComVirgula = 32.767f;
+
+        // double (letra no final opcional) suporta um numero maior de casas decimais (ele é mais preciso) (%f)
+        double numeroComVirgula2 = 32.767;
+
+        // aspas simples (%c)
+        char letra = 'd';
+
+        //aspas duplas (%s)
+        String nome = "davi";
+
+        // ele armazena true ou false (%b)
+        Boolean logic = true;
+
+        System.out.format("byte: %d\nshort: %d\nint: %d\nlong: %d\nfloat: %.3f\ndouble: %.3f\nchar: %c\nString: %s\nboolean: %b\n",
+                pequeno, quasePequeno, quaseGrande, grandao, numeroComVirgula, numeroComVirgula2, letra, nome, logic);
+    }
+}
+
+
+
+
