832ca846a667ea1d853d8658c933cfbc02f5278f
ddavi1 <davigabriel324@gmail.com>
2024-06-27 21:06:14 -0300

    Dia 03

---
+++ aula03/Main.java
@@ -0,0 +1,57 @@
+package aula03;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // OPERADORES MATEMATICOS
+
+         int a = 1;
+         int b = 2;
+
+        // a + b = 4;
+        // a - b = 0;
+        // a * b = 4;
+        // a / b = 1; vai mostrar quociente
+        // a % b = resto da divisão
+
+
+        // OPERADORES DE COMPARAÇÃO
+        // a==b (testa se dois valores são iguais)
+        // != (verifica se dois valores são diferentes)
+        // > (verifica se o valor é maior que o outro)
+        // < (verifica se o valor é menor que o outro)
+        // >=  (verifica se o valor é maior ou igual)
+        // <= (verifica se o valor é menor ou igual)
+
+        // OPERADORES LÓGICOS
+        // && (necessita que os dois sejam atendidos ou true)
+
+        // T e T = T
+        // T e F = F
+        // F e T = F
+        // F e F = F
+
+
+        // || (necessita que apenas 1 seja atendido ou true)
+
+        // T e T = T
+        // T e F = T
+        // F e T = T
+        // F e F = F
+
+        // ! (ele inverte o resultado)
+        // !T = V
+        // !F = V
+
+
+        System.out.println(a<=b);
+
+    }
+
+
+
+
+
+}
